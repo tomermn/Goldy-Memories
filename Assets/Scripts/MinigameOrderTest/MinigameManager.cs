e49151482f104515caeb80f3b2a08372c09f2d17
tomermn <tomer.meidan11@gmail.com>
2023-10-04 14:43:56 +0300

    added the saving option for statistics

--- Assets/Scripts/MinigameOrderTest/MinigameManager.cs
+++ Assets/Scripts/MinigameOrderTest/MinigameManager.cs
@@ -2,6 +2,7 @@ using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -31,6 +32,11 @@ public class MinigameManager : MonoBehaviour
     public Button button2;
     public int pairNumber;
     public int correctAnswer;
+    public float timeDisplay;
+    public float timePressed;
+    public bool toSaveStats;
+
+    private List<PlayerMemoryTestResult> results = new List<PlayerMemoryTestResult>();
 
 
     [SerializeField] public Pair[] pairs;
@@ -74,6 +80,7 @@ public class MinigameManager : MonoBehaviour
         {
             image1.sprite = itemSprite1;
             image2.sprite = itemSprite2;
+            timeDisplay = Time.time;
             button1.interactable = true;
             button2.interactable = true;
         }
@@ -94,43 +101,71 @@ public class MinigameManager : MonoBehaviour
     }
 
 
+
+
+
+    private void RecordResult(bool isCorrect)
+    {
+        string itemName1 = inventory.GetItemByIndex(pairs[pairNumber].first);
+        string itemName2 = inventory.GetItemByIndex(pairs[pairNumber].second);
+        float timePassed = timePressed - timeDisplay;
+
+        PlayerMemoryTestResult result = new PlayerMemoryTestResult(itemName1, itemName2, pairs[pairNumber].first, pairs[pairNumber].second, isCorrect, timePassed);
+        results.Add(result);
+    }
+
     public void OnButton1Click()
     {
-        if (correctAnswer == 0)
-        {
-            //handle
-        }
-        else
-        {
-            //...
-        }
-        pairNumber++;
-        if (pairNumber == pairs.Length)
-        {
-            button1.interactable = false;
-            button2.interactable = false;
-            return;
-        }
-        Pair currentPair = pairs[pairNumber];
-        DisplayNextItems(currentPair.first, currentPair.second);
-
+        timePressed = Time.time;
+        bool isCorrect = (correctAnswer == 0);
+        RecordResult(isCorrect);
+        ContinueToNextPair();
     }
 
     public void OnButton2Click()
     {
-        if (correctAnswer == 1)
+        timePressed = Time.time;
+        bool isCorrect = (correctAnswer == 1);
+        RecordResult(isCorrect);
+        ContinueToNextPair();
+        
+    }
+
+    private void SaveResultsToCSV()
+    {
+        string filePath = Application.dataPath + "/CSVFiles/memory_test_results.csv";
+        using (StreamWriter writer = new StreamWriter(filePath))
         {
-            //handle
+            writer.WriteLine("Item1Name,Item2Name,Item1Index,Item2Index,IsPlayerCorrect,TimePassed");
+
+            foreach (PlayerMemoryTestResult result in results)
+            {
+                string line = $"{result.item1Name},{result.item2Name},{result.item1Index},{result.item2Index},{result.isPlayerCorrect},{result.pressTime}";
+                writer.WriteLine(line);
+            }
         }
-        else
+    }
+
+
+    private void FinishMemoryTest()
+    {
+        if (toSaveStats)
         {
-            //...
+            SaveResultsToCSV();
         }
+         
+
+    }
+
+    public void ContinueToNextPair()
+    {
         pairNumber++;
         if (pairNumber == pairs.Length)
         {
             button1.interactable = false;
             button2.interactable = false;
+            Debug.Log("write to csv");
+            FinishMemoryTest();
             return;
         }
         Pair currentPair = pairs[pairNumber];
---
+++ Assets/Scripts/MinigameOrderTest/PlayerMemoryTestResult.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class PlayerMemoryTestResult
+{
+    public string item1Name;
+    public string item2Name;
+    public int item1Index;
+    public int item2Index;
+    public bool isPlayerCorrect;
+    public float pressTime;
+
+
+    public PlayerMemoryTestResult(string item1Name, string item2Name, int item1Index, int item2Index, bool isPlayerCorrect, float pressTime)
+    {
+        this.item1Name = item1Name;
+        this.item2Name = item2Name;
+        this.item1Index = item1Index;
+        this.item2Index = item2Index;
+        this.isPlayerCorrect = isPlayerCorrect;
+        this.pressTime = pressTime;
+    }
+}
